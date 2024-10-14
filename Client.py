ee9f1b2dc657a1ea185d8d839e7443560dcb1197
Joel Jacob <joeljacob500@gmail.com>
2024-10-14 12:55:41 -0400

    adds handshake between server and client

--- Client.py
+++ Client.py
@@ -1,12 +1,40 @@
-# Establish connection through handshake
+from socket import *
 
-# When connection is accepted, communicate name back to server.
+class Client:
+    # Establish connection through handshake
+    '''DESIGN NOTE: In a real world application the client object will probably be 
+    invoked from another object, hence it makes sense to establish a connection
+    to server only when the client object is constructed
+    '''
+    def __init__(self):
+        self.server_ip = '172.20.152.221' # Run: 'wsl hostname -I' in powershell, to get IP
+        self.server_port = 12000 # Set in server file
+        self.clientSocket = self.establish_connection()
 
-# Send a string to the server, get input through CLI
+    # Returns a socket, that has communication with specified server above
+    ''' DESIGN NOTE: Having a function that creates a socket, allows us to recreate a socket
+        outside of initialization, in the event it is again needed after the connection is closed.
+    '''
+    def establish_connection(self):
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        try:
+            clientSocket.connect((self.server_ip, self.server_port))
+            print("Connection established")
+        except Exception as e: # If server already has 3 clients
+            print(f"Failed to connect: Server is possibly full{e}")
+            return None
+        return clientSocket
 
-# Send a 'status' message to recieve, sever cache info 
+    # When connection is accepted, communicate name back to server.
 
-# Send 'exit' message, to close connection whenever user ready.
+    # Send a string to the server, get input through CLI
 
-# Send 'list' message. When list of files recieved, ask user for name of file they want.
-# If entered file name invalid ask user again.
+    # Send a 'status' message to recieve, sever cache info 
+
+    # Send 'exit' message, to close connection whenever user ready.
+
+    # Send 'list' message. When list of files recieved, ask user for name of file they want.
+    # If entered file name invalid ask user again.
+
+if __name__ == "__main__":
+    client = Client()
--- Server.py
+++ Server.py
@@ -1,16 +1,62 @@
-# Listen for handshake request. Accept no more than 3 clients.
+from socket import *
+import datetime
 
-# Assign client name in format, Client01, Client02, etc
+class Server:
+    def __init__(self):
+        self.server_port = 12000 # Set server port
 
-''' Cache info about accepted clients of current session, (store only in memory): 
-    - Time and date connection was accepted
-    - Time and date connection was finished
-'''
+        self.num_clients = 0 # Amt of connected clients
 
-# Listen for any tcp messages with a string. Once recived echo same string back to client
-# with 'ACK' appended at the end. 
+        # Set up cache in the form of 2d list
+        # Format: [ [client_name, time & date connection started, time & date connection ended]  ]
+        self.cache = []; 
 
-# Listen for an 'exit' message. Upon recieval, terminate connection with that specific client.
+        # List for tracking clients and their ip 
+        # Format: [[client name, ip]]
+        self.clients = []
 
-# Listen for a 'list' message. Upon recieval, give a list of files, and wait for response.
-# Upon response send the clients desired file. If name invalid ask again.
+        # Set up welcoming socket
+        self.server_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_socket.bind(('', self.server_port))
+        self.server_socket.listen(1)
+
+        # Listens and handles requests from clients
+        self.listen()
+
+    # Listen for the type of request and handle accordingly
+    def listen(self):
+        while True:
+            # Handshake request
+            connection_socket, addr = self.server_socket.accept()
+            if not(self.num_clients < 3): # If 3 clients are already connected reject connection
+                connection_socket.close()
+                continue
+            else:
+                date_added = datetime.datetime.now()
+            
+            # Add the client once handshake built
+            self.add_client(addr, date_added)
+             
+    # Updates the clients and cache list attribute to add client
+    def add_client(self, addr, date_added):
+        self.num_clients += 1
+
+        # Create a client name in format Client01, Client02, etc
+        cname = "Client0" + str(self.num_clients)
+
+        # Add to clients list
+        self.clients.append([cname, addr])
+
+        # Add to cache
+        self.cache.append([cname, date_added, None])
+
+    # Listen for any tcp messages with a string. Once recived echo same string back to client
+    # with 'ACK' appended at the end. 
+
+    # Listen for an 'exit' message. Upon recieval, terminate connection with that specific client.
+
+    # Listen for a 'list' message. Upon recieval, give a list of files, and wait for response.
+    # Upon response send the clients desired file. If name invalid ask again.
+
+if __name__ == "__main__":
+    server = Server()
